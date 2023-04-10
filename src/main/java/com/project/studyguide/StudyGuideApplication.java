5646d5261dacf16b0c9f1b6816aa5b33be6ac86a
rodrigo.vidal <rodrigo.vidal@hypeone.com>
2023-04-10 17:37:33 -0300

    Initial commit

---
+++ src/main/java/com/project/studyguide/StudyGuideApplication.java
@@ -0,0 +1,13 @@
+package com.project.studyguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyGuideApplication.class, args);
+    }
+
+}
